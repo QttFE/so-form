82ea3631a3047bc8dc4761444eb6f659ba1e3cb9
王瑞 <wangrui@qutoutiao.net>
2018-10-26 15:49:07 +0800

    feat: update example

--- example/basic/src/App.vue
+++ example/basic/src/App.vue
@@ -5,68 +5,47 @@
     </iForm>
     <button @click="reset">reset</button>
     <button @click="validate">validate</button>
-    <HelloWorld msg="Welcome to Your Vue.js App" />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
   name: 'app',
   data: () => ({
     model: {
-      obj: {
-        input: ''
-      },
-      input: '',
-      select: '',
-      radio: 'a',
-      checkbox: ['a', 'b'],
-      switch: false,
-      slider: 10,
-      datepicker: ''
+      job: '',
+      msg: '',
+      nickname: '',
+      isNickMsg: 1,
+      isNickMsg1: [],
+      land: 1,
     },
     rules: {
-      'obj.input': [
-        { required: true, message: '请输入活动名称', trigger: 'blur' },
-        { min: 3, max: 5, message: '长度在 3 到 5 个字符', trigger: 'blur' }
-      ],
-      select: [
-        { required: true, message: '请选择活动区域', trigger: 'change' }
-      ],
-      checkbox: [
-        { required: true, message: '请选择活动区域', trigger: 'change' }
-      ],
+      'job': [
+          { required: true, message: '请输入活动名称', trigger: 'blur' },
+          { min: 3, max: 5, message: '长度在 3 到 5 个字符', trigger: 'blur' }
+      ]
     }
   }),
-  components: {
-    HelloWorld,
-  },
   computed: {
     columns (h) {
       return [
-        { label: 'input', name: 'obj.input', type: 'input', on: {
-          input: () => {
-            // this.model.slider = 20
-          } }},
-        { slots: {
-            label: h => <span>slot</span>
-          },label: 'select', name: 'select', type: 'select', options: [{id:1,name:'a'}, {id: 2,name: 'b'}] , on: {
-          change:() => {
-            this.model.slider = 20
-          }
+        { label: '任务名称', name: 'job', type: 'input' },
+        { label: '短信内容', name: 'msg', type: 'input', on: {}, props: {
+            type: 'textarea'
         }},
-        { name: 'checkbox', type: 'checkbox', options: ['a', 'b', 'c'] , on: {}},
-        { name: 'radio', type: 'radio', options: ['a', 'b', 'c'] , on: {}},
-        { name: 'switch', type: 'switch'},
-        { name: 'slider', type: 'slider'},
-        { name: 'datepicker', type: 'datepicker'},
-        { name: 'custom', type: 'custom', render (h) {
+        { label: '是否昵称短信', name: 'isNickMsg', type: 'radio', options: [{value: 1, name: '否'}, {value: 2, name:'是'}] , on: {}},
+        { label: '昵称短信内容', name: 'nickname', type: 'input', on: {}, props: {
+            type: 'textarea'
+        }},
+        // label 自定义 render
+        { name: 'land', type: 'radio', options: [{value: 1, name: '否'}, {value: 2, name:'是'}], slots: { label: (h) => (<span style="color: gray">落地页个性化 by render</span>) }},
+        // custom 表单
+        { label: 'cu', name: 'custom', type: 'custom', render (h) {
           return (
-            <span>444</span>
+            <span>自定义内容</span>
           )
-        } },
+        }}
       ]
     }
   },
--- example/basic/src/main.js
+++ example/basic/src/main.js
@@ -2,13 +2,16 @@ import Vue from 'vue'
 import App from './App.vue'
 import Element from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
-import wrapperElement from './formm/wrapper/element'
+// import wrapperElement from './formm/wrapper/element'
 // import qform from 'q-form'
 import qform from '../../../src/install'
 
 Vue.use(Element)
 Vue.use(qform, {
-  source: Element
+  source: Element,
+  components: {
+
+  }
 })
 
 Vue.config.productionTip = false
