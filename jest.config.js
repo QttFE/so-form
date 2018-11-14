669a3b01927994acb63352288e788c6fc1978b4b
39 <ruiru@5921.sina.com>
2018-11-14 16:34:10 +0800

    chore: null

--- jest.config.js
+++ jest.config.js
@@ -2,7 +2,7 @@ module.exports = {
   coverageReporters: ['json', 'lcov', 'text-summary', 'clover'],
   moduleFileExtensions: ['js', 'json', 'vue'],
   transform: {
-       '^.+\\.js$': 'babel-jest',
+    '^.+\\.js$': 'babel-jest',
     '^.+\\.jsx$': 'babel-jest',
     '^.+\\.vue$': 'vue-jest'
   }
