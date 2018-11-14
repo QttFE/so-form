bab37736ad2bc646ed7628314e314ffde9f1eb1e
39 <ruiru@5921.sina.com>
2018-11-14 16:32:44 +0800

    test: test prettier

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,8 @@
 module.exports = {
-  coverageReporters: ["json", "lcov", "text-summary", "clover"],
+  coverageReporters: ['json', 'lcov', 'text-summary', 'clover'],
   moduleFileExtensions: ['js', 'json', 'vue'],
   transform: {
-    '^.+\\.js$': 'babel-jest',
+       '^.+\\.js$': 'babel-jest',
     '^.+\\.jsx$': 'babel-jest',
     '^.+\\.vue$': 'vue-jest'
   }
